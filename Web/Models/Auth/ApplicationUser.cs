322f5fbe33fa0d4d52b0404f92baba97bca0b029
Salem Kosemani <salemsky95@gmail.com>
2021-03-10 23:39:10 +0100

    Add project files.

---
+++ Web/Models/Auth/ApplicationUser.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.Models.Auth
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public byte[] ProfilePicture { get; set; }
+        public string Address { get; set; }
+        public short CountryId { get; set; }
+        [ForeignKey("CountryId")]
+        public Country Country { get; set; }
+    }
+    public class Country
+    {
+        [Key]
+        public short CountryId { get; set; }
+        public string CountryName { get; set; }
+    }
+}
